e53397641f56038e27bddf1cc81fd870e83d3e48
Rustam Saifutdinov <halk2908@yandex.ru>
2018-08-27 18:12:21 +0500

    I changed the user module.

--- index.js
+++ index.js
@@ -2,17 +2,41 @@ var canvas = document.getElementById('sandbox'),
     context = canvas.getContext('2d');
 
 //context.fillRect(50,25, 200, 250);
-    var square=new Path2D();
-square.moveTo(50,50);
-square.lineTo(250,50);
-square.lineTo(250,250);
-square.lineTo(50,250);
-square.lineTo(50,50);
-context.stroke(square);
-    var circle=new Path2D();
-circle.arc(150,150,100,0,2*Math.PI);
-context.lineWidth=5;
-context.strokeStyle="red";
-context.fillStyle="blue";
-context.stroke(circle);
-context.fill(circle);
+var R = 250;
+for(var d=0;d<60;d++){
+    var angel=(d/60)*(2*Math.PI);
+    var pX=Math.cos(angel)*R;
+    var pY=-Math.sin(angel)*R;
+    var qX=0.95*pX;
+    var qY=0.95*pY;
+    pX+=R;pY+=R;
+    qX+=R;qY+=R;
+circle=new Path2D();
+    circle.moveTo(qX,qY);
+    circle.lineTo(pX,pY);
+    context.stroke(circle);
+}
+
+square=new Path2D();
+    square.arc(250,250,250,0,2*Math.PI);
+context.stroke(square)
+
+var date=new Date();
+var hours=date.getHours();
+var minutes=date.getMinutes();
+var seconds=date.getSeconds();
+
+secondsAngel=(seconds/60)*(2*Math.PI);
+minutesAngel=(minutes/60)*(2*Math.PI);
+hoursAngel=((hours%12)/12)*(2*Math.PI);
+
+secondsAngel=Math.PI/2-secondsAngel;
+minutesAngel=Math.PI/2-minutessAngel;
+hoursAngel=Math.PI/2-hoursAngel;
+
+function drawWatch(){
+    context.clearRect(0,0,300,300);
+    setTimeout(drawWatch,1000);
+}
+
+drawWatch();
